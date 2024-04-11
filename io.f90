4b4e0c770c6b8a335d8a50a7a5c25a14c660f73b
David Liptai <dliptai@swin.edu.au>
2024-04-11 22:59:57 +1000

    add in some fake record markers

--- io.f90
+++ io.f90
@@ -10,12 +10,16 @@ contains
     integer :: fh
 
     call open_file_write(filename, fh)
+    call write_fake_recordmarker(fh)
     call write_header(fh, tn, time)
+    call write_fake_recordmarker(fh)
+    call write_fake_recordmarker(fh)
     call write_array_mpi(fh, d, type_mpi_array)
     call write_array_mpi(fh, v1, type_mpi_array)
     call write_array_mpi(fh, v2, type_mpi_array)
     call write_array_mpi(fh, v3, type_mpi_array)
     call write_array_mpi(fh, e, type_mpi_array)
+    call write_fake_recordmarker(fh)
     call close_file(fh)
 
   end subroutine write_hormone_dump
@@ -26,12 +30,16 @@ contains
     integer :: fh
 
     call open_file_read(filename, fh)
+    call read_fake_recordmarker(fh)
     call read_header(fh, tn, time)
+    call read_fake_recordmarker(fh)
+    call read_fake_recordmarker(fh)
     call read_array_mpi(fh, d, type_mpi_array)
     call read_array_mpi(fh, v1, type_mpi_array)
     call read_array_mpi(fh, v2, type_mpi_array)
     call read_array_mpi(fh, v3, type_mpi_array)
     call read_array_mpi(fh, e, type_mpi_array)
+    call read_fake_recordmarker(fh)
     call close_file(fh)
 
   end subroutine read_hormone_dump
