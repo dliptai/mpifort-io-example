b02845002ecd18be957dc23cb672a566ca49df1e
David Liptai <dliptai@swin.edu.au>
2024-04-11 23:23:36 +1000

    Get filename from command-line argument

--- main.f90
+++ main.f90
@@ -8,11 +8,22 @@ program Main
 
   integer :: ierr
   integer :: i,j,k
-  character(len=*), parameter :: filename = 'sodshock_x.bin'
+  character(len=100) :: filename
   real(8), external :: xfunc
   integer :: fh
 
   call init_mpi
+  ! Get filename from command-line argument on master rank
+  if (myrank == 0) then
+    call getarg(1, filename)
+    if (trim(filename) == "") then
+      print*, "No filename provided. Program will terminate."
+      call MPI_ABORT(MPI_COMM_WORLD, 1, ierr)
+    end if
+  end if
+
+  ! Broadcast filename to all ranks
+  call MPI_BCAST(filename, len(filename), MPI_CHARACTER, 0, MPI_COMM_WORLD, ierr)
 
   ! Set bounds of full domain
   is_global=1; ie_global=500
@@ -25,7 +36,7 @@ program Main
   allocate(d(is:ie,js:je,ks:ke))
   allocate(v1,v2,v3,e,mold=d)
 
-  call read_hormone_dump(filename)
+  call read_hormone_dump(trim(filename))
 
   do i = is, ie
     write(myrank+100,*) i, d(i,:,:)
